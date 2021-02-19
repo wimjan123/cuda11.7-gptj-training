fabed8bf633ea145ce656c12f30fbdf9a9814854
Jesus Alvarez <jesusa@nvidia.com>
2021-02-18 17:33:52 -0800

    Use 'set +e' instead of after_script

--- util.sh
+++ util.sh
@@ -14,7 +14,26 @@ retry() {
         fi
     done
     if [[ $counter -gt $maxtries ]]; then
-        >&2 echo FAILED
+        >&2 echo RETRIES FAILED
         return 1
+    fi
+}
+
+kitmaker_cleanup_webhook_success() {
+    if [ ! -z $KITMAKER ] && [ ! -z $TRIGGER ]; then
+        [[ ! -f TAG_MANIFEST ]] && exit 0  # Only call the success webhook in the deploy stage
+        export a_image_name=$(awk '/./{line=$0} END{print line}' TAG_MANIFEST) # get the artifactory repo name
+        sed -i '$ d' TAG_MANIFEST # delete the last line containing the artifactory repo
+        export json_data="{\"status\": \"success\", \"gitlab_pipeline_url\": \"${CI_PIPELINE_URL}\", \"image_name\": \"${a_image_name}\", \"tags\": $(cat TAG_MANIFEST | jq -R . | jq -s . | jq 'map(select(length > 0))' | jq -c .)}"
+        echo curl -v -H "Content-Type: application/json" -d "${json_data}" "${WEBHOOK_URL}"
+        curl -v -H "Content-Type: application/json" -d "${json_data}" "${WEBHOOK_URL}"
+    fi
+}
+
+kitmaker_webhook_failed() {
+    if [ ! -z $KITMAKER ] && [ ! -z $TRIGGER ]; then
+        export json_data="{\"status\": \"failed\", \"gitlab_pipeline_url\": \"${CI_PIPELINE_URL}\"}"
+        echo curl -v -H "Content-Type: application/json" -d "${json_data}" ${WEBHOOK_URL}
+        curl -v -H "Content-Type: application/json" -d "${json_data}" ${WEBHOOK_URL}
     fi
 }
