108d277bec23bd0e8d7b70d57a0f9860169d7615
Jesus Alvarez <jesusa@nvidia.com>
2022-05-06 21:43:07 -0700

    Explicitly install cudnn runtime package in devel images
    closes #160

---
+++ test/08-cudnn7.bats
@@ -0,0 +1,48 @@
+#!/usr/bin/env bats
+
+load helpers
+
+image="${IMAGE_NAME}:${CUDA_VERSION}-cudnn8-devel-${OS}${IMAGE_TAG_SUFFIX}"
+
+function setup() {
+    run docker pull --platform linux/${ARCH} ${image}
+    if [ "$status" -ne 0 ]; then
+        debug "in here yo"
+        skip "'${image}' does not exist"
+    fi
+    check_runtime
+}
+
+function check_ubuntu() {
+    vers=$(docker run --rm --gpus 0 ${image} bash -c "apt show libcudnn8 2>&1 | grep Version | cut -d: -f2 | xargs")
+    [ "$vers" != "" ]
+    debug "runtime version: $vers"
+    vers_dev=$(docker run --rm --gpus 0 ${image} bash -c "apt show libcudnn8-dev 2>&1 | grep Version | cut -d: -f2 | xargs")
+    [ "$vers_dev" != "" ]
+    debug "devel version: $vers_dev"
+    [ "$vers" = "$vers_dev" ]
+}
+
+function check_rhel() {
+    vers=$(docker run --rm --gpus 0 ${image} bash -c "rpm -qa | grep libcudnn8 | grep -v devel")
+    debug "runtime version: $vers"
+    [ "$vers" != "" ]
+    # sed chops out the 'devel-' so it's eaiser to compare
+    vers_dev=$(docker run --rm --gpus 0 ${image} bash -c "rpm -qa | grep libcudnn8 | grep devel | sed 's/devel-//'")
+    [ "$vers_dev" != "" ]
+    debug "devel version ('devel-' pre-stripped): $vers_dev"
+    [ "$vers" = "$vers_dev" ]
+}
+
+@test "check_cudnn8_runtime_package" {
+    debug ${image}
+
+    # RUN WITH
+    #
+    # DEBUG=1
+    #
+    # set in the enviroment for more helpful output!
+
+    [[ "${OS_NAME}" == "ubuntu" ]] && check_ubuntu && return
+    [[ "${OS_NAME}" == "centos" ]] || [[ "${OS_NAME}" == "ubi" ]] || [[ "${OS_NAME}" == "rockylinux" ]] && check_rhel
+}
--- test_manager/test_manager.py
+++ test_manager/test_manager.py
@@ -82,3 +82,76 @@ def test_check_nvprof_install_rhel():
             print(f"has nvprof: {df}")
     if failed:
         pytest.fail("nvprof is not being installed in some rhel-based docker files!")
+
+def get_cudnn_dockefiles(distro):
+    files = []
+    for root, _, _ in os.walk("dist"):
+        if any(x in root for x in ["deprecated", "base"]) or "cudnn" not in root:
+            continue
+        if "devel" not in root:
+            continue
+        if "rhel" not in distro and distro not in root:
+            continue
+        if "rhel" in distro and not any(d in root for d in ["ubi", "centos", "rockylinux"]):
+            continue
+        files.append(pathlib.Path(f"{root}/Dockerfile"))
+    return files
+
+def arches_in_dockerfile(df):
+    count = 0
+    with open(df) as file:
+        lines = file.readlines()
+        for line in lines:
+            if "base as base-" in line:
+                count = count + 1
+    return count
+
+def cudnn_package_check(distro):
+    failed = False
+    for df in get_cudnn_dockefiles(distro):
+        # print(df)
+        ref_count = 0
+        num_arch = arches_in_dockerfile(df)
+        # print(f"number of architectures in dockerfile: {num_arch}")
+        with open(df) as file:
+            lines = file.readlines()
+            for line in lines:
+                if "ENV NV_CUDNN_PACKAGE " in line:
+                    ref_count = ref_count + 1
+                if "ENV NV_CUDNN_PACKAGE_DEV " in line:
+                    ref_count = ref_count + 1
+        if ref_count != num_arch * 2:
+            failed = True
+            print(f"imbalance detected for cudnn: '{df}'...balance calculation is {ref_count} != {num_arch * 2}")
+    if failed:
+        pytest.fail("cudnn explicit install imbalance detected!")
+
+def test_cudnn_devel_has_correct_runtime_package_rhel():
+    """Checks the cudnn devel dockerfile for balanced cudnn packages.
+
+    A dockerfile will have a number of architectures, and each architecture
+    should have a runtime and devel cudnn package explicitly installed.
+
+    If the calculation is imbalanced, then that means something is missing.
+
+    This test runs before the images are built, and there is a second test that
+    is used once the image has been built. See test/09-cudnn8.bats.
+
+    Implemented for https://gitlab.com/nvidia/container-images/cuda/-/issues/160
+    """
+    cudnn_package_check("rhel")
+
+def test_cudnn_devel_has_correct_runtime_package_ubuntu():
+    """Checks the cudnn devel dockerfile for balanced cudnn packages.
+
+    A dockerfile will have a number of architectures, and each architecture
+    should have a runtime and devel cudnn package explicitly installed.
+
+    If the calculation is imbalanced, then that means something is missing.
+
+    This test runs before the images are built, and there is a second test that
+    is used once the image has been built. See test/09-cudnn8.bats.
+
+    Implemented for https://gitlab.com/nvidia/container-images/cuda/-/issues/160
+    """
+    cudnn_package_check("ubuntu")
